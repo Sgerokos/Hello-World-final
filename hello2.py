f53df7a54739d7dcdc9cbdec4a44ac67b3e08f71
Sgerokos <sgerokos@nyit.edu>
2016-04-25 03:40:36 -0400

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,6 +1,5 @@
 # This program says hello and asks for my name.
-int main () {
-  print('Hello World!')
+print('Hello World!')
 print('What is your name?')             # ask for there name
 myName = input()
 print('It is good to meet you,' + myName)
@@ -10,7 +9,4 @@ print('What is your age?')      # ask for there age
 myAge=input()
 print('You will be ' + str(int(myAge) +1)  +  ' in a year.')
 print('Have a nice day ' + myName)
-system ("PAUSE");
-   } // this ends while loop
-return 0;
-} // this ends your main function. 
+
